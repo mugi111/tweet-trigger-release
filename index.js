ca32f71eb8d6425deefce7ea823956bd1d064a77
mugi <m.111_t.1230_nittc@outlook.com>
2020-01-17 09:29:08 +0900

    LIB: twitter APIのインストール

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const core = require('@actions/core');
-const wait = require('./wait');
-
+const Twitter = require('twitter');
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
-
-    core.debug((new Date()).toTimeString())
-    wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
-
-    core.setOutput('time', new Date().toTimeString());
+  try {
+    const client = new Twitter({
+      consumer_key: core.getInput('consumer_key'),
+      consumer_secret: core.getInput('consumer_secret'),
+      access_token_key: core.getInput('access_token_key'),
+      access_token_secret: core.getInput('access_token_secret')
+    });
   } 
   catch (error) {
     core.setFailed(error.message);
